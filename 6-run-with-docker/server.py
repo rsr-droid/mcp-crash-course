d7cbc9034106f6f0b8fc929594ad51b0f5b12645
Rajan Rai <1994rajanrai@gmail.com>
2025-07-20 17:56:38 +0100

    cleanup

--- 6-run-with-docker/server.py
+++ 6-run-with-docker/server.py
@@ -17,6 +17,20 @@ def add(a: int, b: int) -> int:
     """Add two numbers together"""
     return a + b
 
+@mcp.tool()
+def subtract(a: int, b: int) -> int:
+    """Subtract two numbers"""
+    return a - b
+
+@mcp.tool()
+def multiply(a: int, b: int) -> int:
+    """Multiply two numbers"""
+    return a * b
+    
+@mcp.tool()
+def divide(a: int, b: int) -> int:
+    """Divide two numbers"""
+    return a / b
 
 # Run the server
 if __name__ == "__main__":
